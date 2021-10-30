bdd9fef1be40fa3d71cec51f55a18cd49e26ee23
BumbaDavid <dave2000tm@yahoo.com>
2021-10-30 23:39:36 +0300

    frontend

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,72 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping("/products")
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
+	
+	//create product
+	@PostMapping("/products")
+	public Product createProduct(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
+	//get product by id
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Product doesn't exist with id :" + id));
+		return ResponseEntity.ok(product);
+	}
+	
+	//update product
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetail){
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Product doesn't exist with id :" + id));
+		product.setName(productDetail.getName());
+		product.setPrice(productDetail.getPrice());
+		
+		Product updatedProduct = productRepository.save(product);
+		return ResponseEntity.ok(updatedProduct);
+	}
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteProduct(@PathVariable Long id){
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Product doesn't exist with id :" + id));
+		productRepository.delete(product);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		
+		return ResponseEntity.ok(response);
+	}
+}
